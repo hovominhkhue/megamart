12e6453a1b00d3d55c97ef4ba97e9884b07ca626
Ho Vo Minh Khue <hovominhkhue@gmail.com>
2025-09-16 10:19:49 +0200

    Seed de demo

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
@@ -14,10 +15,16 @@ class ProductFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
-    {
+    public function definition(): array {
+        $name = fake()->unique()->words(3, true);
         return [
-            //
+            'category_id' => null,
+            'name' => ucfirst($name),
+            'slug' => Str::slug($name).'-'.fake()->unique()->numberBetween(1000,9999),
+            'price_cents' => fake()->numberBetween(990, 99900),
+            'stock' => fake()->numberBetween(0, 50),
+            'cover_image' => null,
+            'description' => fake()->sentence(12),
         ];
     }
 }
--- database/seeders/DemoSeeder.php
+++ database/seeders/DemoSeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DemoSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        $cats = collect(['Mobile','Cosmetics','Electronics','Furniture','Watches'])
+            ->map(fn($c) => \App\Models\Category::create([
+                'name'=>$c, 'slug'=>Str::slug($c)
+            ]));
+
+        \App\Models\Product::factory(30)->create()->each(function($p) use ($cats){
+            $p->category_id = $cats->random()->id;
+            $p->save();
+        });
     }
 }
